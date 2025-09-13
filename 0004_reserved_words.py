c1be90faf70f42e63103750fdac6b0e4d8d29334
César Torrontegui <cesarbustos871@gmail.com>
2025-09-13 01:41:49 -0600

    docs(0004_reserved_words.py): add keywords vs soft keywords guide

---
+++ 0004_reserved_words.py
@@ -0,0 +1,17 @@
+# Las palabras reservadas son aquellas
+# que tienen un significado especial en el lenguaje de programación 
+# y no pueden ser utilizadas como identificadores (nombres de variables, funciones, clases, etc.).
+
+# 1. Aquí hay una lista de las 35 palabras reservadas (keywords) en Python:
+reserved_words = [
+    "False", "None", "True", "and", "as", "assert", "async", "await",
+    "break", "class", "continue", "def", "del", "elif", "else", "except",
+    "finally", "for", "from", "global", "if", "import", "in", "is",
+    "lambda", "nonlocal", "not", "or", "pass", "raise", "return",
+    "try", "while", "with", "yield"
+]
+
+# 2. Palabras blandas (soft keywords)
+# Las palabras blandas son aquellas que tienen un significado especial en ciertos contextos
+# De la version 3.10 de Python en adelante:
+soft_keywords = [ "match", "case" ]
