e17c514fdef9667ab0035261f6f39e29183b3851
César Torrontegui <cesarbustos871@gmail.com>
2025-09-13 19:01:37 -0600

    Move [f-strings] from [0006_input()_output_basics] to [0007_string_basics]

--- 0006_input_output_basics.py
+++ 0006_input_output_basics.py
@@ -10,16 +10,8 @@ print("Hola, " + nombre + "!")
 edad = input("¿Cuántos años tienes? ")
 print("Tienes", edad, "años.")
 
-# También puedes usar f-strings para formatear la salida.
-# Las f-strings son cadenas de texto que permiten incrustar expresiones dentro de llaves {}.
-# Se definen anteponiendo una 'f' antes de las comillas.
-print(f"{nombre} tiene {edad} años.")
-
 # Los parametroas internos de la funcion print() son:
 # args: los valores a imprimir.
 # sep: define el separador entre los argumentos (por defecto es un espacio).
 # end: define el carácter que se imprime al final (por defecto es un salto de línea).
 print("Python", "es", "genial", sep=" ", end="!\n")
-
-
-
